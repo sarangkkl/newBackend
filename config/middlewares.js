cb2a16b65ca11981f0573f0b09c3125b31df91c5
sarangkkl <sarangkkl2@gmail.com>
2022-06-20 23:31:42 +0530

    pushing now for the final

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,6 @@
 module.exports = ({env})=> [
   'strapi::errors',
+ 
   {
     name: "strapi::security",
     config: {
@@ -24,7 +25,7 @@ module.exports = ({env})=> [
       },
     },
   },
-  'strapi::security',
+  
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -33,4 +34,41 @@ module.exports = ({env})=> [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+ 
 ];
+
+// module.exports = ({ env }) => [
+//   'strapi::errors',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           'connect-src': ["'self'", 'https:'],
+//           'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+//           'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     name: "strapi::body",
+//     config: {
+//       formLimit: "256mb", // modify form body
+//       jsonLimit: "256mb", // modify JSON body
+//       textLimit: "256mb", // modify text body
+//       formidable: {
+//         maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+//       },
+//     },
+//   },
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+  
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,5 @@
 module.exports = ({env}) => ({
-    
+    // ...
     upload: {
       config: {
         provider: "strapi-provider-upload-do", 
@@ -8,9 +8,30 @@ module.exports = ({env}) => ({
           secret: env('DO_SPACE_SECRET_KEY'),
           endpoint: env('DO_SPACE_ENDPOINT'),
           space: env('DO_SPACE_BUCKET'),
+          directory: env('DO_SPACE_DIRECTORY'),
           cdn: env('DO_SPACE_CDN'),
         }
       },
     }, 
-    
+    // ...
   })
+
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       config: {
+//         provider: 'cloudinary',
+//         providerOptions: {
+//           cloud_name: env('CLOUDINARY_NAME'),
+//           api_key: env('CLOUDINARY_KEY'),
+//           api_secret: env('CLOUDINARY_SECRET'),
+//         },
+//         actionOptions: {
+//           upload: {},
+//           uploadStream: {},
+//           delete: {},
+//         },
+//       },
+//     },
+//     // ...
+//   });
